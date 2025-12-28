0986febe69eefc7b5e4b6257a1b348857d7bbf7e
LPX7CFX <dreammimints@gmail.com>
2025-12-28 15:21:05 +0700

    DifficultySorting2

--- Assets/Script/WordStoration.cs
+++ Assets/Script/WordStoration.cs
@@ -62,15 +62,60 @@ public class WordStore : MonoBehaviour
 
     public WordData GetRandomWord()
     {
+
+        WordData word;
         if (remainingWords.Count == 0)
             ResetPool();
 
-        int index = Random.Range(0, remainingWords.Count);
-        WordData word = remainingWords[index];
-        Debug.Log("Get Random Word Activated");
+        
 
-        remainingWords.RemoveAt(index);
-        return word;
+        if (Easy.Count > 0)
+        {
+            int Index = Random.Range(0, Easy.Count);
+            word = Easy[Index];
+            Easy.RemoveAt(Index);
+            remainingWords.Remove(word);
+
+            return word;
+
+        }
+        else if (Medium.Count > 0 && Easy.Count == 0)
+        {
+            int Index = Random.Range(0, Medium.Count);
+            word = Medium[Index];
+            Medium.RemoveAt(Index);
+            remainingWords.Remove(word);
+
+            return word;
+
+        }
+        else if (Hard.Count > 0 && Medium.Count == 0 && Easy.Count == 0)
+        {
+            
+            int Index = Random.Range(0, Hard.Count);
+            word = Hard[Index];
+            Hard.RemoveAt(Index);
+            remainingWords.Remove(word);
+
+            return word;
+
+        }
+       
+        //int index = Random.Range(0, remainingWords.Count);
+        //word = remainingWords[index];
+
+        
+        return null;
+
+    
+        //Debug.Log("Get Random Word Activated");
+
+        //remainingWords.RemoveAt(index);
+        //return word;
+
+        
+
+        
         
     }
 }
