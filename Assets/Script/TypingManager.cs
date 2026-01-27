2ee77b154807f94d8d59acf3073932fd0aba4277
InchI2426 <nachiwat2426@gmail.com>
2026-01-27 17:25:35 +0700

    fix again and again

--- Assets/Script/TypingManager.cs
+++ Assets/Script/TypingManager.cs
@@ -13,6 +13,7 @@ using Unity.VisualScripting;
 
 
 
+
 public class TypingManager : MonoBehaviour
 {
     [SerializeField] RectTransform wordContainer;
@@ -30,9 +31,10 @@ public class TypingManager : MonoBehaviour
     [SerializeField] Button PlayVoice;
     [SerializeField] Button PlayAudio;
     [SerializeField] Button PlaySound;
+    [SerializeField] GameObject CateselGUI;
 
-    [SerializeField] GameObject asd;
-    [SerializeField] GameObject fgh;
+    [SerializeField] private GameObject asd;
+    [SerializeField] private GameObject fgh;
 
 
 
@@ -40,10 +42,12 @@ public class TypingManager : MonoBehaviour
     [SerializeField] GameObject IntroScene;
     [SerializeField] GameObject gamescene;
     [SerializeField] GameObject modescene;
+    public int T = 0;
+    public int C = 0;
 
 
     private List<Letterui> letters = new();
-    private int currentIndex = 0;
+    [SerializeField] private int currentIndex = 0;
 
     private int CountingSys = 1;
     private WordData sss;
@@ -71,7 +75,7 @@ public class TypingManager : MonoBehaviour
 
         wordStore.ResetPool();
 
-        Challenge.onClick.AddListener(StartNextWord);
+        Challenge.onClick.AddListener(activaterC);
 
         Training.onClick.AddListener(training);
         PlayAudio.onClick.AddListener(PlaySoundTraining);
@@ -85,6 +89,7 @@ public class TypingManager : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("Kok" + letters.Count);
 
         if (startnextwordcontrollertrain == 5 && currentIndex >= letters.Count)
         {
@@ -314,6 +319,7 @@ public class TypingManager : MonoBehaviour
         ClearCurrentWord();
         Debug.Log("ClearCurrentWordStartNextWord");
 
+
         WordData wordData = wordStore.GetRandomWord();
         sss = wordData;
         asd.SetActive(false);
@@ -385,12 +391,13 @@ public class TypingManager : MonoBehaviour
 
     }
 
-    void training()
+    public void training()
     {
         Debug.Log("ClearCurrentWordTraining");
 
         asd.SetActive(true);
         fgh.SetActive(false);
+
 
         ClearCurrentWord();
         wordStore.countingsht2++;
@@ -436,6 +443,21 @@ public class TypingManager : MonoBehaviour
 
 
 
+
+    }
+    public void activaterT()
+    {
+        Debug.Log("ActivaterT");
+        T++;
+        CateselGUI.SetActive(true);
+
+
+    }
+    public void activaterC()
+    {
+        Debug.Log("ActivaterC");
+        C++;
+        CateselGUI.SetActive(true);
 
     }
 
